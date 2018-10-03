c052563ee5647263b1d145a486400ef511c56ded
Nate-Weeks <nweeks87@gmail.com>
2018-10-02 21:48:27 -0400

    whole project without uploads

---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"Nweeks","$P$BreSjjvYKKLE8mndCVQrPAXvjUntiN1","nweeks","nweeks@flywheel.local","","2018-05-17 15:42:32","",0,"Nweeks");
---
+++ app/sql/local.wp_woocommerce_attribute_taxonomies.sql
@@ -0,0 +1,6 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_woocommerce_attribute_taxonomies` VALUES
+(1,"size","Size","select","menu_order",1),
+(2,"color","Color","select","menu_order",1);
